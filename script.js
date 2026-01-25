31a4e9969861a47bb07f62e6fe11f0309afb9b57
unknown <denysetiawan619@gmail.com>
2026-01-25 22:40:03 +0700

    updtate

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// togle class active tadinya gak ada jadi ada dan sebaliknya
+// togle class active tadinya gak ada jadi ada dan sebaliknya untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 //ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+//togle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
 // klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
